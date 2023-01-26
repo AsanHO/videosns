4a4a8d70d89f07bf9f1b3ff25ebdf94f22894d07
AsanHO <gorma00@naver.com>
2023-01-26 19:48:47 +0900

    project init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:tictok_clone/constants/gaps.dart';
+import 'package:tictok_clone/screens/sign_up_screen.dart';
 
 void main() {
-  runApp(const TikTokApp());
+  runApp(const TicTokApp());
 }
 
-class TikTokApp extends StatelessWidget {
-  const TikTokApp({super.key});
+class TicTokApp extends StatelessWidget {
+  const TicTokApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TicTok Clone',
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
