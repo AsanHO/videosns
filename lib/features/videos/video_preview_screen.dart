95cb76c60b6e18907a59dd626da7de6b654b3d2a
AsanHO <gorma00@naver.com>
2023-07-11 19:27:09 +0900

    save video

--- lib/features/videos/video_preview_screen.dart
+++ lib/features/videos/video_preview_screen.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:video_player/video_player.dart';
+import "package:gallery_saver/gallery_saver.dart";
 
 class VideoPreviewScreen extends StatefulWidget {
   final XFile preview;
@@ -13,6 +15,7 @@ class VideoPreviewScreen extends StatefulWidget {
 }
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  bool _isSaved = false;
   late final VideoPlayerController _videoPlayerController;
   Future<void> _initVideo() async {
     _videoPlayerController =
@@ -26,15 +29,33 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   @override
   void initState() {
     // TODO: implement initState
+
     super.initState();
     _initVideo();
+  }
+
+  Future<void> _saveGallary() async {
+    if (_isSaved) return;
+    await GallerySaver.saveVideo(widget.preview.path, albumName: "프리뷰");
+    _isSaved = true;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(title: const Text("미리보기")),
+      appBar: AppBar(
+        title: const Text("미리보기"),
+        actions: [
+          IconButton(
+            onPressed: _saveGallary,
+            icon: _isSaved
+                ? const FaIcon(FontAwesomeIcons.check)
+                : const FaIcon(FontAwesomeIcons.download),
+          ),
+        ],
+      ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)
           : null,
